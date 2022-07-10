f724d73604952d173e0dadbbc37c952207ab8407
Dan <dan@shefro.local>
2022-07-10 12:09:28 -0400

    Add in the List and IDs APIs for blockservice
    The pull request merged from master only had Create and Destroy. List and IDs are easy, so add them too.

--- twitter/block.go
+++ twitter/block.go
@@ -51,3 +51,48 @@ func (s *BlockService) Destroy(params *BlockDestroyParams) (User, *http.Response
 	resp, err := s.sling.New().Post("destroy.json").QueryStruct(params).Receive(users, apiError)
 	return *users, resp, relevantError(err, *apiError)
 }
+
+// BlockListParams are the parameters for BlockService.List.
+type BlockListParams struct {
+	IncludeEntities *bool  `url:"include_entities,omitempty"`
+	SkipStatus      *bool  `url:"skip_status,omitempty"`
+	Cursor          *int64 `url:"cursor,omitempty"`
+}
+
+// BlockListResponse is the response from BlockService.List
+type BlockListResponse struct {
+	PreviousCursor    int64  `json:"previous_cursor"`
+	PreviousCursorStr string `json:"previous_cursor_str"`
+	NextCursor        int64  `json:"next_cursor"`
+	NextCursorStr     string `json:"next_cursor_str"`
+	Users             []User `json:"users"`
+}
+
+func (s *BlockService) List(params *BlockListParams) (BlockListResponse, *http.Response, error) {
+	blr := new(BlockListResponse)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Post("list.json").QueryStruct(params).Receive(blr, apiError)
+	return *blr, resp, relevantError(err, *apiError)
+}
+
+// BlockIDsParams are the parameters for BlockService.IDs
+type BlockIDsParams struct {
+	StringifyIDs *bool  `url:"stringify_ids,omitempty"`
+	Cursor       *int64 `url:"cursor,omitempty"`
+}
+
+// BlockIDsResponse is the response from BlockService.IDs
+type BlockIDsResponse struct {
+	NextCursor        int64   `json:"next_cursor"`
+	NextCursorStr     string  `json:"next_cursor_str"`
+	PreviousCursor    int64   `json:"previous_cusor"`
+	PreviousCursorStr string  `json:"previous_cursor_str"`
+	IDs               []int64 `json:"ids"`
+}
+
+func (s *BlockService) IDs(params *BlockIDsParams) (BlockIDsResponse, *http.Response, error) {
+	r := new(BlockIDsResponse)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Post("ids.json").QueryStruct(params).Receive(r, apiError)
+	return *r, resp, relevantError(err, *apiError)
+}
